581b5e23219bbf834cd63ffb8fa1cc9f208b2402
Pavel Ershov <devnulpavel@gmail.com>
2019-06-17 13:26:41 +0300

    New commentary

--- Example_2/main.py
+++ Example_2/main.py
@@ -17,16 +17,24 @@ def mse(old_val, new_val):
 
 class PartyNN(object):
     def __init__(self, learning_rate=0.1):
+        # Случайные веса перехода из входа на 1й слой, нормальное распределение (колокол),
+        # центр распределения в 0.0, минимум и максимум - (2 ** -0.5 = 0.7071067811865476)?
+        # 3 входа, 2 выхода
         self.weights_0_1 = np.random.normal(0.0, 2 ** -0.5, (2, 3))
+        # Случайные веса перехода из 1го слоя на выход, нормальное распределение (колокол),
+        # центр распределения в 0.0, минимум и максимум - (-1,1)?
+        # 2 входа, 1 выход
         self.weights_1_2 = np.random.normal(0.0, 1, (1, 2))
+        # Обертка, котора применяет функцию sigmoid к вектору значений
         self.sigmoid_mapper = np.vectorize(sigmoid)
+        # Скорость обучения
         self.learning_rate = np.array([learning_rate])
 
     # Метод прямого прохода нейронной сети
     def predict(self, inputs):
         # Выполняем скалярное перемножение весов первого слоя и входных значений
         inputs_1 = np.dot(self.weights_0_1, inputs)
-        # Получаем значения пороговых функций для каждого значения, полученного из весов -
+        # Получаем значения пороговой функции для каждого значения, полученного из весов,
         # результат - это значение выхода из первого слоя
         outputs_1 = self.sigmoid_mapper(inputs_1)
 
@@ -35,6 +43,7 @@ class PartyNN(object):
         # Получаем значения пороговых функций для каждого значения, полученного из весов -
         # результат - это значение выхода из второго слоя
         outputs_2 = self.sigmoid_mapper(inputs_2)
+
         return outputs_2
 
     # Метод тренировки нейронной сети
@@ -58,6 +67,7 @@ class PartyNN(object):
 
 
 def main():
+    # Массив с правильными значениями на которых надо тренироваться
     train = [
         ([0, 0, 0], 0),
         ([0, 0, 1], 1),
@@ -74,19 +84,35 @@ def main():
     epochs = 50000
     learning_rate = 0.08
 
+    # Создаем сеть
     network = PartyNN(learning_rate=learning_rate)
 
+    # Количество итераций обучения
     for e in range(epochs):
         inputs_ = []
         correct_predictions = []
+
+        # Перебираем массив верных значений и результатов для обучения
         for input_stat, correct_predict in train:
-            network.train(np.array(input_stat), correct_predict)
-            inputs_.append(np.array(input_stat))
+            input_array = np.array(input_stat)
+
+            # Тренируем сеть
+            network.train(input_array, correct_predict)
+
+            # Сохраняем верный вход и верный выход для прямого прохода
+            inputs_.append(input_array)
             correct_predictions.append(np.array(correct_predict))
 
-        if e % 1000 == 0:
-            train_loss = mse(network.predict(np.array(inputs_).T), np.array(correct_predictions))
-            sys.stdout.write("Progress: {}, Training loss: {}\n".format(str(100 * e / float(epochs))[:4], str(train_loss)[:6]))
+        if e % (epochs/10) == 0:
+            # Выполняем прямой проход
+            current_result = network.predict(np.array(inputs_).T)
+
+            # Сравниваем текущий результат и верный результат квадратичной функцией
+            train_loss = mse(current_result, np.array(correct_predictions))
+
+            # Выводим текущую ошибку
+            progress = str(100 * e / float(epochs))[:4]
+            sys.stdout.write("Progress: {}, training loss: {}\n".format(progress, str(train_loss)[:6]))
 
     for input_stat, correct_predict in train:
         print("For input: {} the prediction is: {}, expected: {}".format(
@@ -94,8 +120,9 @@ def main():
             str(network.predict(np.array(input_stat))),
             str(correct_predict == 1)))
 
-    #print(str(network.weights_0_1))
-    #print(str(network.weights_1_2))
+    # Выведем веса нашей обученной сети
+    print("Layer 0->1 weights:\n{}\n".format(str(network.weights_0_1)))
+    print("Layer 1->2 weights:\n{}\n".format(str(network.weights_1_2)))
 
 
 if __name__ == '__main__':
